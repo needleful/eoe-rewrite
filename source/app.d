7308ad2efeadb83878dd3e33ec60821bcc181caa
needleful <needleful@outlook.com>
2023-02-17 14:37:50 -0800

    Created a black window

--- source/app.d
+++ source/app.d
@@ -1,6 +1,20 @@
 import std.stdio;
 
+import lantana.input;
+import lantana.render;
+
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	Input input;
+	Window window = Window(640, 480, "At the Ends of Eras");
+	window.grabMouse(false);
+
+	writeln("At the Ends of Eras");
+
+	while(!window.state[WindowState.CLOSED]) {
+		window.pollEvents(&input);
+		window.beginFrame();
+		window.endFrame();
+	}
+	writeln("Exiting...");
 }
